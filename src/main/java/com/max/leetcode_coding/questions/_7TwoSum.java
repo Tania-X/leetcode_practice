dbc02629b8dedf085000170e74e07d1be08d7109
max <13126565350@163.com>
2025-01-21 10:08:01 +0800

    two sum

---
+++ src/main/java/com/max/leetcode_coding/questions/_7TwoSum.java
@@ -0,0 +1,26 @@
+package com.max.leetcode_coding.questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _7TwoSum {
+
+  public static void main(String[] args) {
+
+
+  }
+
+  public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      // 这里有个思维上的惯性：认为在`map`中必须以数组的`index`作为`key`，数值为`value`。实际上反过来完全没问题。
+      if (map.containsKey(target - nums[i])) {
+        return new int[]{i, map.get(target - nums[i])};
+      } else {
+        map.put(nums[i], i);
+      }
+    }
+    throw new RuntimeException("no such nums");
+  }
+
+}
