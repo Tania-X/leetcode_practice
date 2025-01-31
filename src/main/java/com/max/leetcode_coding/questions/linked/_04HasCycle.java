237e55599ec6baa43ff2e95fed81c17da5ec8ff5
max <13126565350@163.com>
2025-01-31 23:43:51 +0800

    judge if a linked list has a circle

---
+++ src/main/java/com/max/leetcode_coding/questions/linked/_04HasCycle.java
@@ -0,0 +1,22 @@
+package com.max.leetcode_coding.questions.linked;
+
+public class _04HasCycle {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static boolean hasCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
