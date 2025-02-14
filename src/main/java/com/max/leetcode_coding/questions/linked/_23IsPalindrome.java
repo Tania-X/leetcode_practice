eeaefe8c2a62cb55c870ae367119c9a24811a01b
max <13126565350@163.com>
2025-02-14 10:01:36 +0800

    check if the given linked list is a palindrome, another method to solve

--- src/main/java/com/max/leetcode_coding/questions/linked/_23IsPalindrome.java
+++ src/main/java/com/max/leetcode_coding/questions/linked/_23IsPalindrome.java
@@ -6,11 +6,15 @@ import java.util.Deque;
 public class _23IsPalindrome {
 
   public static void main(String[] args) {
-    boolean palindrome = isPalindrome(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(1)))));
+    ListNode head = new ListNode(1, new ListNode(2, new ListNode(2)));
+    boolean palindrome = isPalindrome(head);
     System.out.println("palindrome = " + palindrome);
+    boolean palindrome2 = isPalindrome2(head);
+    System.out.println("palindrome = " + palindrome2);
 
   }
 
+  // 借助栈实现，空间复杂度为o(n)，需要断开链表，反而复杂
   public static boolean isPalindrome(ListNode head) {
     Deque<Integer> stack = new ArrayDeque<>();
     ListNode mid = findMiddleNode(head, stack);
@@ -33,6 +37,42 @@ public class _23IsPalindrome {
     }
     if (fast != null) {
       slow = slow.next;
+    }
+    return slow;
+  }
+
+  // 不考虑断开，空间复杂度为o(1)，无论长度为奇数还是偶数
+  public static boolean isPalindrome2(ListNode head) {
+    ListNode mid = findMid(head);
+    ListNode head2 = reverse(mid);
+    while (head2 != null) {
+      if (head2.val != head.val) {
+        return false;
+      }
+      head = head.next;
+      head2 = head2.next;
+    }
+    return true;
+  }
+
+  private static ListNode reverse(ListNode head) {
+    ListNode cur = head;
+    ListNode tail = null;
+    while (cur != null) {
+      ListNode next = cur.next;
+      cur.next = tail;
+      tail = cur;
+      cur = next;
+    }
+    return tail;
+  }
+
+  private static ListNode findMid(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
     }
     return slow;
   }
