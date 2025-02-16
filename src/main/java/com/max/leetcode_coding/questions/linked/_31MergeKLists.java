5fade56cdea17a596f8cdb3d6189a94d9f6caa07
max <13126565350@163.com>
2025-02-16 23:04:12 +0800

    divide and conquer

--- src/main/java/com/max/leetcode_coding/questions/linked/_31MergeKLists.java
+++ src/main/java/com/max/leetcode_coding/questions/linked/_31MergeKLists.java
@@ -8,6 +8,9 @@ public class _31MergeKLists {
     ListNode result = mergeKLists(lists);
     System.out.println("result = " + result);
 
+    ListNode result2 = mergeKLists2(lists);
+    System.out.println("result2 = " + result2);
+
 
   }
 
@@ -22,6 +25,46 @@ public class _31MergeKLists {
       ans = mergeTwoLists(ans, cur);
     }
     return ans;
+  }
+
+  public static ListNode mergeKLists2(ListNode[] lists) {
+    int len = lists.length;
+    if (len == 0) {
+      return null;
+    }
+    return mergeTwoLists(lists, 0, len);
+  }
+
+  private static ListNode mergeTwoLists(ListNode[] lists, int l, int r) {
+    int m = r - l;
+    if (m == 1) {
+      return lists[l];
+    }
+    ListNode l1 = mergeTwoLists(lists, l, l + m / 2);
+    ListNode l2 = mergeTwoLists(lists, l + m / 2, r);
+    return doMergeTwoList(l1, l2);
+  }
+
+  private static ListNode doMergeTwoList(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode();
+    ListNode cur = dummy;
+    while (l1 != null && l2 != null) {
+      if (l1.val <= l2.val) {
+        cur.next = l1;
+        l1 = l1.next;
+      } else {
+        cur.next = l2;
+        l2 = l2.next;
+      }
+      cur = cur.next;
+    }
+    if (l1 == null) {
+      cur.next = l2;
+    }
+    if (l2 == null) {
+      cur.next = l1;
+    }
+    return dummy.next;
   }
 
   private static ListNode mergeTwoLists(ListNode p0, ListNode pi) {
