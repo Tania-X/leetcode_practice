10a1289400f6052fe179d395223b799609e68990
max <13126565350@163.com>
2025-02-22 23:55:36 +0800

    similar pairs, a lit better

--- src/main/java/com/max/leetcode_coding/daily/_02SimilarPairs.java
+++ src/main/java/com/max/leetcode_coding/daily/_02SimilarPairs.java
@@ -1,15 +1,16 @@
 package com.max.leetcode_coding.daily;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class _02SimilarPairs {
 
   public static void main(String[] args) {
-
 
   }
 
@@ -32,6 +33,25 @@ public class _02SimilarPairs {
         }
       }
     }
+    return ans;
+  }
+
+  public static int similarPairs2(String[] words) {
+    int ans = 0;
+    Map<Set<Character>, Integer> map = new HashMap<>();
+    for (String word : words) {
+      char[] charArray = word.toCharArray();
+      Set<Character> set = new HashSet<>();
+      for (char c : charArray) {
+        set.add(c);
+      }
+      map.merge(set, 1, Integer::sum);
+    }
+    Collection<Integer> values = map.values();
+    for (Integer value : values) {
+      ans += value * (value - 1) / 2;
+    }
+
     return ans;
   }
 
