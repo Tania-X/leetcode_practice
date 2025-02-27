950b7ae1e65731547d6ba9feb4c1735b8c09f8dd
max <13126565350@163.com>
2025-02-27 17:17:47 +0800

    count fair pairs better way

--- src/main/java/com/max/leetcode_coding/binarySearch/_04CountFairPairs.java
+++ src/main/java/com/max/leetcode_coding/binarySearch/_04CountFairPairs.java
@@ -8,7 +8,7 @@ public class _04CountFairPairs {
 
   }
 
-  // 其实不需要复制数组
+  // 其实不需要复制数组，下面的解答更好
   public static long countFairPairs(int[] nums, int lower, int upper) {
     long ans = 0;
     int[] sup = new int[nums.length];
@@ -31,6 +31,31 @@ public class _04CountFairPairs {
   public static int lowerBound(int[] nums, int target) {
     int left = -1;
     int right = nums.length;
+    while (left + 1 < right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] >= target) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+    return right;
+  }
+
+  public static long countFairPairs2(int[] nums, int lower, int upper) {
+    long ans = 0;
+    Arrays.sort(nums);
+    for (int j = 0; j < nums.length; j++) {
+      // 保证 i < j
+      int start = lowerBound(nums, j, lower - nums[j]);
+      int end = lowerBound(nums, j, upper - nums[j] + 1) - 1;
+      ans += end - start + 1;
+    }
+    return ans;
+  }
+
+  public static int lowerBound(int[] nums, int right, int target) {
+    int left = -1;
     while (left + 1 < right) {
       int mid = left + (right - left) / 2;
       if (nums[mid] >= target) {
