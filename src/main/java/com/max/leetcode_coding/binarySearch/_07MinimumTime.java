c91ce4c4182ab385820b07269dded729210267aa
max <13126565350@163.com>
2025-02-28 17:05:52 +0800

    minimum time to finish bus trips

---
+++ src/main/java/com/max/leetcode_coding/binarySearch/_07MinimumTime.java
@@ -0,0 +1,34 @@
+package com.max.leetcode_coding.binarySearch;
+
+public class _07MinimumTime {
+
+  public static void main(String[] args) {
+    long l = minimumTime(new int[]{5, 10, 10}, 9);
+    System.out.println("l = " + l);
+  }
+
+  public static long minimumTime(int[] time, int totalTrips) {
+    long right = -1;
+    for (int j : time) {
+      right = Math.max(right, j);
+    }
+    right *= totalTrips;
+    long ans = right;
+    long left = 0;
+    while (left + 1 < right) {
+      long t = left + (right - left) / 2;
+      long sum = 0;
+      for (int j : time) {
+        sum += t / j;
+      }
+      if (sum >= totalTrips) {
+        right = t;
+        ans = Math.min(t, ans);
+      } else {
+        left = t;
+      }
+    }
+    return ans;
+  }
+
+}
