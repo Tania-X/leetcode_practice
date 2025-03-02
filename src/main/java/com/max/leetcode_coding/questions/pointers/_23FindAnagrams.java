e6bc3634d9f8b7991b84d6a9e5aa49e8615a5ee3
max <13126565350@163.com>
2025-03-02 23:04:31 +0800

    fixed slide windows solution to finding anagrams

--- src/main/java/com/max/leetcode_coding/questions/pointers/_23FindAnagrams.java
+++ src/main/java/com/max/leetcode_coding/questions/pointers/_23FindAnagrams.java
@@ -1,6 +1,7 @@
 package com.max.leetcode_coding.questions.pointers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,6 +47,33 @@ public class _23FindAnagrams {
       }
     }
     return true;
+  }
+
+  // 定长的滑动窗口
+  public static List<Integer> findAnagrams2(String s, String p) {
+    int n = s.length();
+    List<Integer> ans = new ArrayList<>();
+    int[] sArray = new int[26];
+    int[] pArray = new int[26];
+    char[] chars = p.toCharArray();
+    char[] source = s.toCharArray();
+    for (char c : chars) {
+      pArray[c - 'a']++;
+    }
+    for (int right = 0; right < n; right++) {
+      sArray[source[right] - 'a']++;
+      int left = right - p.length() + 1;
+      // 窗口长度不足则跳过
+      if (left < 0) {
+        continue;
+      }
+      if (Arrays.equals(pArray, sArray)) {
+        ans.add(left);
+      }
+      // 窗口左侧移出
+      sArray[source[left] - 'a']--;
+    }
+    return ans;
   }
 
 }
