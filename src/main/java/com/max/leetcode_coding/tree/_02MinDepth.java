f42a862ed419bf9eb67d36f219a75503f48cedcf
max <13126565350@163.com>
2025-03-03 17:51:17 +0800

    bottom-up solution to find min depth in a binary tree

---
+++ src/main/java/com/max/leetcode_coding/tree/_02MinDepth.java
@@ -0,0 +1,28 @@
+package com.max.leetcode_coding.tree;
+
+public class _02MinDepth {
+
+  public static void main(String[] args) {
+
+  }
+
+  private static int ans = Integer.MAX_VALUE;
+
+  public static int minDepth(TreeNode root) {
+    dfs(root, 0);
+    return ans == Integer.MAX_VALUE ? 0 : ans;
+  }
+
+  private static void dfs(TreeNode node, int depth) {
+    if (node == null) {
+      return;
+    }
+    depth++;
+    if (node.left == null && node.right == null) {
+      ans = Math.min(ans, depth);
+    }
+    dfs(node.left, depth);
+    dfs(node.right, depth);
+  }
+
+}
