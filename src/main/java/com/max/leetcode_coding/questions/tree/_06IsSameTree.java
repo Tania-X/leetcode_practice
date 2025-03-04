2baacbff6e4c0f6fec27605e97e0b83a938afe44
max <13126565350@163.com>
2025-03-04 22:44:02 +0800

    check if the two trees are of the same tree

---
+++ src/main/java/com/max/leetcode_coding/questions/tree/_06IsSameTree.java
@@ -0,0 +1,12 @@
+package com.max.leetcode_coding.questions.tree;
+
+public class _06IsSameTree {
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null || q == null) {
+      return p == q;
+    }
+    return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
+}
