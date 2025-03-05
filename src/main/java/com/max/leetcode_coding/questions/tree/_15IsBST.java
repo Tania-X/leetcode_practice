dd1714dce8c059f59212ccfc7bd3b081648d4d60
max <13126565350@163.com>
2025-03-05 23:32:16 +0800

    in-order traversal solution as well as post-order traversal solution to check if a tree is a valid binary search tree

--- src/main/java/com/max/leetcode_coding/questions/tree/_15IsBST.java
+++ src/main/java/com/max/leetcode_coding/questions/tree/_15IsBST.java
@@ -12,7 +12,41 @@ public class _15IsBST {
       return true;
     }
     int x = node.val;
-    return x < right && x > left && isValidBST(node.left, left, x) && isValidBST(node.right, x, right);
+    return x < right && x > left && isValidBST(node.left, left, x) && isValidBST(node.right, x,
+        right);
+  }
+
+  private long pre = Long.MIN_VALUE;
+
+  // in-order traversal
+  public boolean isValidBST2(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    if (!isValidBST2(root.left) || root.val <= pre) {
+      return false;
+    }
+    pre = root.val;
+    return isValidBST2(root.right);
+  }
+
+  // post-order traversal
+  public boolean isValidBST3(TreeNode root) {
+    return dfs(root)[1] != Long.MAX_VALUE;
+  }
+
+  private long[] dfs(TreeNode node) {
+    // attention here: we return such an array in order that the caller knows it's null, and it quickly works through
+    if (node == null) {
+      return new long[]{Long.MAX_VALUE, Long.MIN_VALUE};
+    }
+    long[] left = dfs(node.left);
+    long[] right = dfs(node.right);
+    int x = node.val;
+    if (x <= left[1] || x >= right[0]) {
+      return new long[]{Long.MIN_VALUE, Long.MAX_VALUE};
+    }
+    return new long[]{Math.min(left[0], x), Math.max(right[1], x)};
   }
 
 }
