b6d2377c614a749f0590122f7032defd3c203ad5
max <13126565350@163.com>
2025-03-05 13:55:58 +0800

    flip equiv

---
+++ src/main/java/com/max/leetcode_coding/questions/tree/_11FlipEquiv.java
@@ -0,0 +1,16 @@
+package com.max.leetcode_coding.questions.tree;
+
+public class _11FlipEquiv {
+
+  public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+    if (root1 == null || root2 == null) {
+      return root1 == root2;
+    }
+    if (root1.val != root2.val) {
+      return false;
+    }
+    return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (
+        flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+  }
+
+}
