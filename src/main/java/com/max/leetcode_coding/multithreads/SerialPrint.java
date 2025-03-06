825369dc1c7767f0e2733a109ab5a216f8541271
max <13126565350@163.com>
2025-03-06 17:49:56 +0800

    serial print

---
+++ src/main/java/com/max/leetcode_coding/multithreads/SerialPrint.java
@@ -0,0 +1,31 @@
+package com.max.leetcode_coding.multithreads;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SerialPrint {
+
+  private final AtomicBoolean firstDone = new AtomicBoolean(false);
+
+  private final AtomicBoolean secondDone = new AtomicBoolean(false);
+
+  public void first(Runnable printFirst) throws InterruptedException {
+    printFirst.run();
+    firstDone.set(true);
+  }
+
+  public void second(Runnable printSecond) throws InterruptedException {
+    while (!firstDone.get()) {
+      // do nothing
+    }
+    printSecond.run();
+    secondDone.set(true);
+  }
+
+  public void third(Runnable printThird) throws InterruptedException {
+    while (!secondDone.get()) {
+      // do nothing
+    }
+    printThird.run();
+  }
+
+}
