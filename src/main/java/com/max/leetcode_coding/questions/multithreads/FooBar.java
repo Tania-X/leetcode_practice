7b53bc041502399749c9c2e55dd0edf6c684dc6b
max <13126565350@163.com>
2025-03-07 12:25:10 +0800

    foo bar~

--- src/main/java/com/max/leetcode_coding/questions/multithreads/FooBar.java
+++ src/main/java/com/max/leetcode_coding/questions/multithreads/FooBar.java
@@ -1,45 +1,47 @@
 package com.max.leetcode_coding.questions.multithreads;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.Semaphore;
 
-public class FooBar {
+class Foo {
 
-  private int n;
+  public Foo() {
 
-  private final AtomicBoolean flg = new AtomicBoolean(false);
-
-  public FooBar(int n) {
-    this.n = n;
   }
 
-  public void foo(Runnable printFoo) throws InterruptedException {
+  private final Semaphore semaphore1 = new Semaphore(1);
 
-    for (int i = 0; i < n; i++) {
-      synchronized (this) {
-        while (flg.get()) {
-          this.wait();
-        }
-        // printFoo.run() outputs "foo". Do not change or remove this line.
-        printFoo.run();
-        flg.set(true);
-        this.notify();
-      }
+  private final Semaphore semaphore2 = new Semaphore(0);
+
+  private final Semaphore semaphore3 = new Semaphore(0);
+
+  public void first(Runnable printFirst) throws InterruptedException {
+    try {
+      semaphore1.acquire();
+      // printFirst.run() outputs "first". Do not change or remove this line.
+      printFirst.run();
+    } finally {
+      semaphore2.release();
     }
   }
 
-  public void bar(Runnable printBar) throws InterruptedException {
-
-    for (int i = 0; i < n; i++) {
-      synchronized (this) {
-        while (!flg.get()) {
-          this.wait();
-        }
-        // printBar.run() outputs "bar". Do not change or remove this line.
-        printBar.run();
-        flg.set(false);
-        this.notify();
-      }
+  public void second(Runnable printSecond) throws InterruptedException {
+    try {
+      semaphore2.acquire();
+      // printSecond.run() outputs "second". Do not change or remove this line.
+      printSecond.run();
+    } finally {
+      semaphore3.release();
     }
   }
 
+  public void third(Runnable printThird) throws InterruptedException {
+    try {
+      semaphore3.acquire();
+      // printThird.run() outputs "third". Do not change or remove this line.
+      printThird.run();
+    } finally {
+      semaphore1.release();
+    }
+
+  }
 }
