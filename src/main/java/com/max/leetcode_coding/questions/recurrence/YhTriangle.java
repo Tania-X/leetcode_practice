b304358cef0ef762fe7e36c917008d6dd62e0083
max <13126565350@163.com>
2025-03-19 11:08:11 +0800

    trivial adjustment: recurrence -> memorization -> dynamic programming

--- src/main/java/com/max/leetcode_coding/questions/recurrence/YhTriangle.java
+++ src/main/java/com/max/leetcode_coding/questions/recurrence/YhTriangle.java
@@ -46,7 +46,6 @@ public class YhTriangle {
 
   public static int yhTriangle3(int m, int n) {
     int[][] dp = new int[m][n];
-    dp[0][0] = 1;
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
         if (i == 0 || j == 0 || j == i) {
